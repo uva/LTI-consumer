f28d0a65dfeff25916e3d04d47ab854e5d3738bc
Gerrit Oomens <g.oomens@uva.nl>
2023-02-18 19:09:45 +0100

    Use token in example project

--- UvA.LTI.Example/Program.cs
+++ UvA.LTI.Example/Program.cs
@@ -1,7 +1,23 @@
 using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using UvA.LTI;
 
+var key = "blawlaekltjwelkrjtwlkejlekwjrklwejr32423";
+var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key));
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddAuthorization()
+    .AddAuthentication(opt => opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt =>
+    {
+        opt.TokenValidationParameters.ValidateAudience = false;
+        opt.TokenValidationParameters.ValidIssuer = "lti";
+        opt.TokenValidationParameters.IssuerSigningKey = signingKey;
+    });
+
 var app = builder.Build();
 
 app.UseLti(new LtiOptions
@@ -9,7 +25,7 @@ app.UseLti(new LtiOptions
     ClientId = "104400000000000213",
     AuthenticateUrl = "https://uvadlo-dev.test.instructure.com/api/lti/authorize_redirect",
     JwksUrl = "https://canvas.test.instructure.com/api/lti/security/jwks",
-    SigningKey = "blawlaekltjwelkrjtwlkejlekwjrklwejr32423",
+    SigningKey = key,
     ClaimsMapping = c => new Dictionary<string, object>
     {
         [ClaimTypes.NameIdentifier] = c.NameIdentifier,
@@ -17,7 +33,13 @@ app.UseLti(new LtiOptions
     }
 });
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseDefaultFiles();
 app.UseStaticFiles();
+
+app.MapGet("Test", r => r.Response.WriteAsJsonAsync(r.User.Claims.Select(c => new { c.Type, c.Value })))
+    .RequireAuthorization();
 
 app.Run();
