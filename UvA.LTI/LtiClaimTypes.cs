d3065961d0bd1ac38c80de555b1472d731ae70c5
Roy <50085581+rvankoten@users.noreply.github.com>
2023-04-03 08:15:16 +0200

    Claims resolver (#1)
    * Add Lis claim
    
    * Add a claims resolver using DI
    
    * Add example using the claims resolver
    
    * Update version and readme

--- UvA.LTI/LtiClaimTypes.cs
+++ UvA.LTI/LtiClaimTypes.cs
@@ -5,4 +5,5 @@ public static class LtiClaimTypes
     public const string Roles = "https://purl.imsglobal.org/spec/lti/claim/roles";
     public const string Context = "https://purl.imsglobal.org/spec/lti/claim/context";
     public const string Custom = "https://purl.imsglobal.org/spec/lti/claim/custom";
+    public const string Lis = "https://purl.imsglobal.org/spec/lti/claim/lis";
 }
